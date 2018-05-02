276e3dce6d9c40cb9e7675b1b62bebeff1e301ac
xxf <email@xxf.com>
2018-05-02 15:10:34 +0800

    ...修改

--- src/com/xxf/controller/Test.java
+++ src/com/xxf/controller/Test.java
@@ -3,6 +3,7 @@ package com.xxf.controller;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world...");
+		System.out.println("Hello world...");
 	}
 }
